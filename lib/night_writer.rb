ae363adb8c265fb0767831a41cea148bb82ab38a
Andra Helton <andrahelton@gmail.com>
2023-01-13 19:56:37 -0700

    Add function for file changes based on second command line input

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,4 +3,9 @@ handle = File.open(ARGV[0], "r")
 incoming_text = handle.read
 handle.close
 
-puts incoming_text
+# puts incoming_text
+
+reversed = incoming_text.reverse
+writer = File.open(ARGV[1], "w")
+writer.write(reversed)
+writer.close
