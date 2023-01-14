f3e628a4b9571c1b99628576376679cb0b80ed17
Andra Helton <andrahelton@gmail.com>
2023-01-14 14:08:50 -0700

    Add attributes passing test for translator

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,2 +1,7 @@
 class Translator
+  attr_reader :message
+  def initialize(message)
+    @message = message
+  end
+  
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,15 +1,15 @@
 require './lib/translator'
 
 RSpec.describe Translator do
-  let(:translator) { Translator.new }
+  let(:translator) { Translator.new("H") }
 
   describe "#initialize" do
     it "exists" do
       expect(translator).to be_an_instance_of(Translator)
     end
 
-    xit "#attributes" do
-
+    it "#attributes" do
+      expect(translator.message).to eq("H")
     end
   end
 end
