13bc81e0bde002907ef98e7ae9531e47de05ac14
Andra Helton <andrahelton@gmail.com>
2023-01-15 18:08:51 -0700

    Add passing test for converts space character to :space

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,21 @@ include Dictionary
     @message = message
   end
 
+
   def translate
     @message.chars.map do |character|
-      DICTIONARY[character.to_sym]
+      if character == " "
+        DICTIONARY[:space]
+      else
+        DICTIONARY[character.to_sym]
+      end
     end.join("\n")
   end
+  
+  # def translate
+  #   @message.chars.map do |character|
+  #     DICTIONARY[character.to_sym]
+  #   end.join("\n")
+  # end
 
 end
