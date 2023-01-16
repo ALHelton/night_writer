5fcd3152a4c1276c9a580f85dc80e44c5eac1754
Andra Helton <andrahelton@gmail.com>
2023-01-16 12:01:49 -0700

    Add passing test for #reorder

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,22 +17,20 @@ include Dictionary
     end
   end
 
-  # def translate
-  #   translated = @message.chars.map do |character|
-  #     if character == " "
-  #       DICTIONARY[:space].split("\n")
-  #     else
-  #       DICTIONARY[character.to_sym].split("\n")
-  #       require 'pry'; binding.pry
-  #     end
-
-  #     tier1 = translated[0]
-  #     tier2 = translated[1]
-  #     tier3 = translated[2]
-  #   end
-
-
-
-  # end
-
+  def reorder
+    tier1 = [].join("")
+    tier2 = [].join("")
+    tier3 = [].join("")
+    translate.map do |letter|
+      divided = letter.split("\n")
+      tier1 << divided[0]
+      tier2 << divided[1]
+      tier3 << divided[2] 
+    end
+    all_tiers = []
+    all_tiers << tier1
+    all_tiers << tier2
+    all_tiers << tier3
+    all_tiers
+  end
 end
