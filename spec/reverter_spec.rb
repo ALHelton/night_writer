656900940ce7d27cb01a2508d4b81f0449fe0de3
Andra Helton <andrahelton@gmail.com>
2023-01-15 21:24:30 -0700

    Add passing test for translates multiple braille words to English

--- spec/reverter_spec.rb
+++ spec/reverter_spec.rb
@@ -4,6 +4,7 @@ require './lib/reverter'
 RSpec.describe Reverter do
   let(:reverter) { Reverter.new("0.\n00\n..") }
   let(:reverterplus) { Reverter.new("0.\n..\n..\n0.\n0.\n..\n00\n..\n..") }
+  let(:reverterultra) { Reverter.new("0.\n00\n..\n.0\n0.\n..\n..\n..\n..\n00\n.0\n00\n0.\n.0\n0.\n0.\n..\n00") }
 
   describe "#initialize" do
     it "exists" do
@@ -22,6 +23,10 @@ RSpec.describe Reverter do
 
     it "translates multiple Braille letters back to English" do
       expect(reverterplus.revert).to eq("abc")
+    end
+
+    it "translates multiple braille words back to English" do
+      expect(reverterultra.revert).to eq("hi you")
     end
   end
 end
