7f236f8754e203186d7b78c45891a6d7fb482531
Andra Helton <andrahelton@gmail.com>
2023-01-16 17:56:52 -0700

    Add adjustments to test for #text_wrap

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -49,10 +49,11 @@ RSpec.describe Translator do
   end
 
   describe "#text_wrap" do
-    xit "takes formatted text and sends it to a new line after 80 characters" do
+    it "takes formatted text and sends it to a new line after 80 characters" do
       translatormagnum.text_wrap
 
       firstlines = ".00.0...000..0000...0.0.0..000..000.00...00.0000.0..0.0.0.0....00.0...0.0.0.00..\n0000.0..00..0.......0.00.000.0..0..0....00....0.0....00..000..0000.0..0....0.0..\n0.......0.00....0.....0.0..00.....0.00....000.0.0...0.00..0...0.......0...0000..\n\n"
+
       secondlines = "000.00\n.0.000\n..0..."
 
       expect(translatormagnum.translate).to eq([firstlines, secondlines])
